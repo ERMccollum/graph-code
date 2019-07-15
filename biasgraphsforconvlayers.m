d936e12bb541d0974c9e8f843bf3211fc3482e06
ERMccollum <51455824+ERMccollum@users.noreply.github.com>
2019-07-15 17:02:06 -0500

    Add files via upload

--- biasgraphsforconvlayers.m
+++ biasgraphsforconvlayers.m
@@ -1,20 +1,23 @@
 
-for j = [1 4 7 10 13 16 20 22 24 25 27 29 30 32 34 35 37 39]
+for layernumber = [1 4 7 10 13 16 20 22 24 25 27 29 30 32 34 35 37 39]
     
-biasmatrix = net.Layers(j).Bias;
-z = size(net.Layers(j).Bias);
+biasmatrix = net.Layers(layernumber).Bias;
+z = size(net.Layers(layernumber).Bias);
 q = z(:);
-h = q(4);
+channelnumber = q(4);
 
-for k = 1:h
+for k = 1:channelnumber
     x(k) = k;
     y(k) = biasmatrix(:,:,1,k);
   
     
    
 end
-figure (j) 
-plot(x, y)
-ylim([-10,10])
+figure (layernumber) 
 
+plot(x, y)
+title(strcat( 'Bias Layer ' , " " , num2str(layernumber) ))
+ylim([-10,10])
+xlim([1,channelnumber])
+saveas(figure(layernumber),fullfile(pwd,['BiasGraphLayerNumber' num2str(layernumber)]));
 end 
